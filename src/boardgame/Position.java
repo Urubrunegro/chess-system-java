1a204b50ab6c75f86360615701f0cfa4ffaf789c
Flavio Freire <urubrunegro@gmail.com>
2024-03-21 21:35:15 +0000

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,45 @@
+package boardgame;
+
+public class Position {
+
+    private Integer row;
+    private Integer column;
+
+    public Position(){
+
+    }
+
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Position{");
+        sb.append("row=").append(row);
+        sb.append(", column=").append(column);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public void setValues(int row, int column){
+
+    }
+}
